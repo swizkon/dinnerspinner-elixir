a06526bb5b36fabd8001438f69022833659e8054
Jonas Jerndin <jonas@queenslab.se>
2021-05-17 22:59:24 +0200

    Fix first query

--- src/dinner_spinner_umbrella/apps/dinner_spinner/lib/dinners/dinner_queries.ex
+++ src/dinner_spinner_umbrella/apps/dinner_spinner/lib/dinners/dinner_queries.ex
@@ -1,11 +1,9 @@
-defmodule DinnerSpinner.DinnerQueries
+defmodule DinnerSpinner.DinnerQueries do
     import Ecto.Query
 
-    alias DinnerSpinner.[Repo, Dinners]
+    alias DinnerSpinner.{Repo, Dinners}
 
-    
     def get_all do
          Repo.all(from Dinners)
     end
-    
 end
