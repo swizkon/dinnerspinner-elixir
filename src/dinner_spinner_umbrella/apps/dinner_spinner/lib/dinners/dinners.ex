fcd9c24e319415c0b9781658334d6f16cd123af2
Jonas Jerndin <jonas@queenslab.se>
2021-05-19 23:32:44 +0200

    Add validation for dinner model

--- src/dinner_spinner_umbrella/apps/dinner_spinner/lib/dinners/dinners.ex
+++ src/dinner_spinner_umbrella/apps/dinner_spinner/lib/dinners/dinners.ex
@@ -1,11 +1,22 @@
 defmodule DinnerSpinner.Dinners do
     use Ecto.Schema
 
+    import Ecto.Changeset
+
     schema "dinners" do
         field :title, :string
         field :ingredients, :string
 
-        timestamps
+        timestamps()
     end
 
+    @required_fields ~w(title)a    
+    @optional_fields ~w(ingredients)a
+
+    def changeset(dinner, params \\ %{}) do
+        dinner
+        |> cast(params, @required_fields ++ @optional_fields)
+        |> validate_required(@required_fields)
+    end
+    
 end
